56b5515a5729622e2aa89defa8206ed83d5ad547
SMYALTAMASH <smy.altamash@gmail.com>
2021-04-15 14:19:47 +0530

    changing metadata file

--- buildproxyvalidator.sh
+++ buildproxyvalidator.sh
@@ -8,4 +8,4 @@ node=$2
 org=$3
 
 docker build -f ./Dockerfile --label commitHash=$(git rev-parse --short HEAD) -t ${org}/${name}:${build_tag} .
-echo {\"proxyvalidator_image_name\" : \"${name}\", \"proxyvalidator_image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > proxyvalidatormetadata.json
+echo {\"image_name\" : \"${name}\", \"image_tag\" : \"${build_tag}\", \"node_name\" : \"$node\"} > metadata.json
